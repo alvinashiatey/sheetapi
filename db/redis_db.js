483b204b6cb1bf8e5dcb05a9f5caca5ab3875c7f
Alvin Ashiatey <alvinashiatey@gmail.com>
2022-01-13 23:37:22 -0500

    run production with pm2

--- db/redis_db.js
+++ db/redis_db.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 const { createClient } = require("redis");
-let redis = createClient({ url: process.env.REDIS_URL });
+let redis = createClient({
+  url: process.env.REDIS_URL,
+  password:
+    process.env.NODE_ENV === "production"
+      ? process.env.REDIS_PASSWORD
+      : undefined,
+});
 redis.on("connect", function (err, response) {
   "use strict";
   console.log("Connected to database");
